fafec441c5724f1998c29b2afbb828c21c70604d
Iryste <jean-baptiste.hamel@hotmail.fr>
2024-07-25 09:16:39 +0200

    logincheck

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Login = () => {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [usernameError, setUsernameError] = useState();
+    const [passwordError, setPasswordError] = useState();
+
+    const onButtonClick = () => {
+        if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(username)) {
+            setUsernameError('Veuillez saisir une adresse mail valide, ex:john.doeuf@monsite.fr');
+            return;
+        }
+        if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$.!%*?&])[A-Za-z\d@$!%*.?&]{9,}$/.test(password)) {
+            setPasswordError('Veuillez saisir un mot de passe valide');
+            return;
+        }
+        // loginCheck()
+    }
+
     return (
 <form className={'mainContainer'}>
     <div className={'titleContainer'}>
@@ -8,17 +26,17 @@ const Login = () => {
     </div>
     <br />
     <div className={'inputContainer'}>
-        <input  type="text" placeholder="Saisir votre adresse mail" className={'inputBox'} autoComplete="username" />
-        <label className="errorLabel"></label>
+        <input  type="text" placeholder="Saisir votre adresse mail" className={'inputBox'} autoComplete="username" onChange={(e) => setUsername(e.target.value)} />
+        <label className="errorLabel">{usernameError}</label>
     </div>
     <br />
     <div className={'inputContainer'}>
-        <input type="password" placeholder="Saisir votre mot de passe" className={'inputBox'} autoComplete="current-password" />
-        <label className="errorLabel"></label>
+        <input type="password" placeholder="Saisir votre mot de passe" className={'inputBox'} autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} />
+        <label className="errorLabel">{passwordError}</label>
     </div>
     <br />
     <div className={'inputContainer'}>
-        <input className={'inputButton'} type="button" value={'Se connecter'} />
+        <input className={'inputButton'} onClick={onButtonClick} type="button" value={'Se connecter'} />
     </div>
     </form>
     );
